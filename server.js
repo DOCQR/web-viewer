ee73c670b371e12db3f5504e0799351ac540a38e
nmattise <n.mattise@gmail.com>
2015-09-27 09:16:42 -0400

    raw body

--- server.js
+++ server.js
@@ -2,18 +2,18 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
 
-var morgan       = require('morgan');
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
-
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var getRawBody = require('raw-body');
 var configDB = require('./config/database.js');
 
 // configuration ===============================================================
@@ -24,20 +24,33 @@ require('./config/passport')(passport); // pass passport for configuration
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-// app.use(bodyParser.json()); // get information from html forms
-// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json({limit: '200mb'}));
-app.use(bodyParser.raw({limit: '200mb'}));
-app.use(bodyParser.text({limit: '200mb'}));
-app.use(bodyParser.urlencoded({limit: '200mb', extended: true}));
-
+app.use(bodyParser.json({
+  limit: '200mb'
+}));
+app.use(bodyParser.urlencoded({
+  limit: '200mb',
+  extended: true
+}));
+app.use(function(req, res, next) {
+  getRawBody(req, {
+    length: req.headers['content-length'],
+    limit: '1mb',
+    encoding: typer.parse(req.headers['content-type']).parameters.charset
+  }, function(err, string) {
+    if (err) return next(err)
+    req.text = string
+    next()
+  })
+})
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({
+  secret: 'ilovescotchscotchyscotchscotch'
+})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
