89f10eb27c8c166fe8adb34970c2e919f3402083
nmattise <n.mattise@gmail.com>
2015-09-27 08:06:52 -0400

    bigger files

--- server.js
+++ server.js
@@ -24,8 +24,11 @@ require('./config/passport')(passport); // pass passport for configuration
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser.json()); // get information from html forms
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json()); // get information from html forms
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json({limit: '200mb'}));
+app.use(bodyParser.urlencoded({limit: '200mb', extended: true}));
 
 app.use(express.static('public'));
 
